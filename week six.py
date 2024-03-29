de583daa78e761d83f4c91f7afefb21c59e52641
dackmatta <makambientryx@gmail.com>
2024-03-29 18:34:29 +0300

    file handling week-6

--- week six.py
+++ week six.py
@@ -3,9 +3,16 @@
  #   for y in range(3):
   #      print(f'({x}, {y})')
 
-numbers=[1,1,1,1,6,]
-for x_count in numbers:
-    output=''
-    for count in range(x_count):
-        output+='x'
-    print(output)
+#numbers=[1,1,1,1,6,]
+#for x_count in numbers:
+ #   output=''
+  #  for count in range(x_count):
+   #     output+='x'
+    #print(output)
+
+numbers=[1,2,3,4,5,7,8,7,4,5]
+unique=[]
+for number in numbers:
+    if number not in unique:
+     unique.append(number)
+print(unique)
