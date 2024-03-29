ee711924d85c8d6957b1851d28884242a8585f99
dackmatta <makambientryx@gmail.com>
2024-03-29 12:18:10 +0300

    file handling week-6

---
+++ week six.py
@@ -0,0 +1,11 @@
+#Nested Loop
+#for x in range(4):
+ #   for y in range(3):
+  #      print(f'({x}, {y})')
+
+numbers=[1,1,1,1,6,]
+for x_count in numbers:
+    output=''
+    for count in range(x_count):
+        output+='x'
+    print(output)
